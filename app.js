bf6cbf51c6c0f67c3c65bb29e7aaa42972520b67
FrontElvin <rustamove629@gmail.com>
2024-05-28 20:57:11 +0300

    Финальный пуш

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
-// app.js 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const apiRouter = require("./routes/apiRouter");
+const pagesRouter = require("./routes/pages");
 
-const PORT = 3000;
+const connectToDatabase = require("./database/connect");
+const cors = require("./middlewares/cors");
+
 const app = express();
+const PORT = 3001;
+
+connectToDatabase();
 
 app.use(
-    bodyParser.json(),
-    express.static(path.join(__dirname, 'public')),
-); 
+  cors,
+  cookieParser(),
+  bodyParser.json(),
+  apiRouter,
+  pagesRouter,
+  express.static(path.join(__dirname, "public"))
+);
 
-app.listen(PORT, () => {
-    console.log(`Server is running at PORT http://localhost:${PORT}`);
-})
-     
+app.listen(PORT);
