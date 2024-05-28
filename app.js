006fd2440bd165cb4007bce0a2897d82dba53ac3
FrontElvin <rustamove629@gmail.com>
2024-05-28 18:24:25 +0300

    Еще немного

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const bodyParser = require('body-parser');
 const PORT = 3000;
 const app = express();
 
-app.use(
-    bodyParser.json(),
-    express.static(path.join(__dirname, 'public')),
-); 
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT http://localhost:${PORT}`);
